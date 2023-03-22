f3a56f3b104ffd81807118019f06282abf64c443
Gurjit Sangha <gsangha494@gmail.com>
2023-03-22 12:57:44 +0000

    load image from cloudinary

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,7 @@ import Image from 'next/image';
 import db from '@/lib/db';
 import Link from 'next/link';
 import HeroSection from '@/components/HeroSection';
-
-const truncateString = (str: string, len: number): string => {
-  if (str.length <= len) return str;
-  return str.slice(0, len) + '...';
-};
+import PostCard from '@/components/PostCard';
 
 const getData = async () => {
   const posts = await db.post.findMany({
@@ -24,26 +20,13 @@ export default async function Home() {
       <HeroSection />
       <section className="grid sm:grid-cols-2 lg:grid-cols-3 w-full gap-4 mb-8" id="posts">
         {dbPosts.map((post) => (
-          <Link
-            href={`/post/${post.id}`}
-            className="rounded shadow-lg hover:scale-105 transition-transform"
+          <PostCard
             key={post.id}
-          >
-            <div className="w-full">
-              <Image
-                src={post.hero}
-                width="300"
-                height="300"
-                alt={post.title}
-                sizes="100w"
-                className="w-full h-auto"
-              />
-            </div>
-            <div className="px-4 py-2">
-              <h2 className="text-xl font-bold">{post.title}</h2>
-              <p>{truncateString(post.content || '', 50)}</p>
-            </div>
-          </Link>
+            id={post.id}
+            title={post.title}
+            content={post.content}
+            hero={'cld-sample-5'}
+          />
         ))}
       </section>
     </main>
---
+++ components/PostCard.tsx
@@ -0,0 +1,34 @@
+import { CloudinaryImage } from '@cloudinary/url-gen';
+import { fill } from '@cloudinary/url-gen/actions/resize';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const truncateString = (str: string, len: number): string => {
+  if (str.length <= len) return str;
+  return str.slice(0, len) + '...';
+};
+
+export default function PostCard({ id, title, content, hero }) {
+  const cldImage = new CloudinaryImage(hero, {
+    cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  }).resize(fill().width(300).height(300));
+
+  return (
+    <Link href={`/post/${id}`} className="rounded shadow-lg hover:scale-105 transition-transform">
+      <div className="w-full">
+        <Image
+          src={cldImage.toURL()}
+          width="300"
+          height="300"
+          alt={title}
+          sizes="100w"
+          className="w-full h-auto"
+        />
+      </div>
+      <div className="px-4 py-2">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p>{truncateString(content || '', 50)}</p>
+      </div>
+    </Link>
+  );
+}
